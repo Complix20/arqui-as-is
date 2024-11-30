56f319f8ed43d58be5034aa74f3e678bdb383150
AlejandroC <alexcabero@hotmail.com>
2024-11-29 23:02:11 -0500

    modificaciones

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-provider "local" {}
-
 module "legacy_applications" {
   source = "./modules/servers"
   name   = "Legacy Applications Server"
@@ -10,7 +8,7 @@ module "legacy_applications" {
     size = 300
   }
   os      = "Windows Server 2012 R2"
-  db_type = "SQL Server 2008 R2"
+  db_type = "SQL Server 2008 R2" # Define el tipo de base de datos
 }
 
 module "oracle_erp" {
@@ -24,35 +22,4 @@ module "oracle_erp" {
   }
   os      = "Red Hat 7.9"
   db_type = "Oracle 12.2"
-}
-
-module "crm" {
-  source = "./modules/servers"
-  name   = "Microsoft Dynamics CRM Server"
-  cpu    = 4
-  ram    = 96
-  storage = {
-    type = "Local"
-    size = 1000
-  }
-  os = "Windows Server 2012 R2"
-}
-
-module "network" {
-  source          = "./modules/network"
-  mpls_bandwidth  = "5Mbps"
-  vpn_connections = ["Supplier VPN"]
-  internet_speed  = "60Mbps"
-}
-
-module "backup" {
-  source = "./modules/servers"
-  name   = "Backup Server"
-  cpu    = 24
-  ram    = 128
-  storage = {
-    type = "RAID5"
-    size = 30000
-  }
-  os = "Red Hat 7.9"
 }
--- modules/servers/servers.tf
+++ modules/servers/servers.tf
@@ -5,7 +5,7 @@ resource "local_file" "server_config" {
   RAM: ${var.ram} GB
   Storage: ${jsonencode(var.storage)}
   Operating System: ${var.os}
-  Database Type: ${var.db_type}
+  Database Type: ${var.db_type != null ? var.db_type : "No database"}
   EOT
   filename = "${var.name}_config.txt"
 }
